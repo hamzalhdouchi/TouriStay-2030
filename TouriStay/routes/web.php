87ae796c1a1c40bec15eb7a319255a7b69f180c3
hamzalhadouchi <hamzabniss16@gmail.com>
2025-02-25 17:14:19 +0000

    crée le route de adminController et userController

--- TouriStay/routes/web.php
+++ TouriStay/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\propertiesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome')->middleware("auth");
@@ -26,14 +29,40 @@ Route::get('/inscrer', function () {
     return view('auth.register');
 })->name('inscrer');
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
+
+
+// // Route::get('/dashboard', function () {
+// //     return view('dashboard');
+
 // })->middleware(['auth', 'verified'])->name('dashboard');
+Route::post('/store',[userController::class,'store'])->name('create_user');
+Route::post('/login', [UserController::class, 'login'])->name('user.login');
+Route::get('/dashboard', [AdminController::class, 'read'])->name('user.read');
+Route::post('/properties', [propertiesController::class, 'create'])->name('properties.create');
+Route::get( '/properties', [propertiesController::class, 'readPropretis'])->name('properties.read');
+Route::post( '/modifer/{id}', [propertiesController::class, 'propertiesById'])->name('propertie.modifer');
+Route::delete( '/destroy/{id}', [propertiesController::class, 'destroy'])->name('propertie.destroy');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/logout', [UserController::class, 'logout'])->name('user.logout');
+    Route::delete('/user/{id}', [AdminController::class, 'destroy'])->name('user.destroy');
+    Route::put('/status/{id}', [AdminController::class, 'changeStatus'])->name('user.changeStatus');
+    Route::get('/Profile', [AdminController::class, 'readById'])->name('user.Profile');
+    Route::put( '/user/{id}', [UserController::class, 'update'])->name('user.changeProfile');
 });
+
+
+Route::post('/store',[userController::class,'store'])->name('create_user');
+Route::post('/login', [UserController::class, 'login'])->name('user.login');
+Route::get('/dashboard', [AdminController::class, 'read'])->name('user.read');
+Route::post('/properties', [propertiesController::class, 'create'])->name('properties.create');
+Route::get( '/properties', [propertiesController::class, 'readPropretis'])->name('properties.read');
+Route::post( '/modifer/{id}', [propertiesController::class, 'propertiesById'])->name('propertie.modifer');
+Route::delete( '/destroy/{id}', [propertiesController::class, 'destroy'])->name('propertie.destroy');
+
 
 require __DIR__.'/auth.php';
