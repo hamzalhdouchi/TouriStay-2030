00b5231064715b7c8df9b1ea855f5240179026f6
hamzalhadouchi <hamzabniss16@gmail.com>
2025-02-25 14:35:45 +0000

    add client imgration

--- TouriStay/database/migrations/2025_02_25_143517_client.php
+++ TouriStay/database/migrations/2025_02_25_143517_client.php
@@ -3,8 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
-
 
 return new class extends Migration
 {
@@ -13,10 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::statement("CREATE TABLE admin (
-            role_id INT CHECK (role_id = 3)
-        ) INHERITS (users);");
-        DB::statement("ALTER TABLE admin ADD PRIMARY KEY (user_id);");
+        //
     }
 
     /**
